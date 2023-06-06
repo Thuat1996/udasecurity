172abdfb03e0005dda91e9241a4f7a458a4fb2ec
Thuat1996 <nguyenvanthuat96bn@gmail.com>
2023-06-06 22:50:54 +0700

    Complete project

--- Security/src/main/java/module-info.java
+++ Security/src/main/java/module-info.java
@@ -5,10 +5,6 @@ module com.udacity.catpoint {
     requires com.google.gson;
     requires java.prefs;
     requires com.google.common;
-    requires software.amazon.awssdk.services.rekognition;
-    requires software.amazon.awssdk.auth;
-    requires software.amazon.awssdk.regions;
-    requires software.amazon.awssdk.core;
     opens com.udacity.catpoint.data to com.google.gson;
     opens com.udacity.catpoint.service;
 }
